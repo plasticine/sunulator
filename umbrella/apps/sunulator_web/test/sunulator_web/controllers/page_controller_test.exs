f78b8f787222edbb3436ec60c5fb2f33151a5334
Justin Morris <desk@pixelbloom.com>
2019-07-20 15:13:23 +1000

    Fix spec

--- umbrella/apps/sunulator_web/test/sunulator_web/controllers/page_controller_test.exs
+++ umbrella/apps/sunulator_web/test/sunulator_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule SunulatorWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Sunulator"
   end
 end
